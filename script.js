f1fa5ba722350865632c3ae8a52b1346878bf483
Wesley Parastchuk <wesleyparastchuk13@gmail.com>
2023-10-19 19:33:37 -0300

    19-10

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-let matriz = [];
-
-function generateInitialMatriz(matriz) {
-	for (let i = 0; i < 9; i++) {
-        let row = [];
-		for (let j = 0; j < 9; j++) {
-			row.push((j + i * 3 + Math.floor(i / 3)) % 9 + 1);
-		}
-		matriz.push(row);
-	}
+function createEmptyRow(length = 9) {
+	return Array(length).fill(0);
 }
 
-generateInitialMatriz(matriz);
+function createEmptyMatrix(rows = 9, cols = 9) {
+	return Array(rows)
+		.fill()
+		.map(() => createEmptyRow(cols));
+}
 
-console.table(matriz);
+function arrOneToNine() {
+	return Array.from({ length: 9 }, (_, i) => i + 1);
+}
+
+function randomMatrix() {
+	const resultMatriz = createEmptyMatrix();
+	for (let i = 0; i < 3; i++) {
+		let numberPool = arrOneToNine().sort(() => Math.random() - 0.5);
+		for (let j = 0; j < 3; j++) {
+			for (let k = 0; k < 3; k++) {
+				resultMatriz[j + i * 3][k + i * 3] = numberPool[k + j * 3];
+			}
+		}
+	}
+	return resultMatriz;
+}
+
+let matrix = randomMatrix();
+
+console.table(matrix);
